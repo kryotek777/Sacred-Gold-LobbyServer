8c79df950dd34c9afdfb810c9e0f4900c84ce2d5
Giulio Carlo D'Antonio <kde@kryotek.mozmail.com>
2025-03-30 16:42:09 +0200

    Feat: Made the Database thread-safe

--- src/Database/Database.cs
+++ src/Database/Database.cs
@@ -7,133 +7,193 @@ namespace Lobby.DB;
 public static class Database
 {
     private static SQLiteConnection connection = null!;
+    private static ReaderWriterLockSlim rwLock = new(LockRecursionPolicy.SupportsRecursion);
 
     public static void Load()
     {
         if (!Config.Instance.StorePersistentData)
             return;
 
-        var dbPath = Config.Instance.DatabasePath;
+        rwLock.EnterWriteLock();
+        try
+        {
+            var dbPath = Config.Instance.DatabasePath;
 
-        Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
-        Directory.CreateDirectory(Path.GetDirectoryName(Config.Instance.TemplatePath)!);
-        Directory.CreateDirectory(Path.GetDirectoryName(Config.Instance.SavesPath)!);
+            Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+            Directory.CreateDirectory(Path.GetDirectoryName(Config.Instance.TemplatePath)!);
+            Directory.CreateDirectory(Path.GetDirectoryName(Config.Instance.SavesPath)!);
 
-        if (!File.Exists(dbPath))
-            SQLiteConnection.CreateFile(dbPath);
+            if (!File.Exists(dbPath))
+                SQLiteConnection.CreateFile(dbPath);
 
-        connection = new SQLiteConnection($"Data Source={dbPath}");
+            connection = new SQLiteConnection($"Data Source={dbPath}");
 
-        connection.Open();
+            connection.Open();
 
-        CreateTables();
+            CreateTables();
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
+        }
     }
 
     public static void Close()
     {
-        connection.Close();
+        rwLock.EnterWriteLock();
+        try
+        {
+            connection.Close();
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
+        }
     }
 
     public static Account CreateAccount(string username, string password)
     {
-        string idQuery = @"SELECT MAX(PermId) FROM Accounts";
+        rwLock.EnterWriteLock();
+        try
+        {
+            string idQuery = @"SELECT MAX(PermId) FROM Accounts";
 
-        using var command = new SQLiteCommand(idQuery, connection);
+            using var command = new SQLiteCommand(idQuery, connection);
 
-        var result = command.ExecuteScalar();
+            var result = command.ExecuteScalar();
 
-        var permId = result != DBNull.Value ? (int)(long)result : 0;
+            var permId = result != DBNull.Value ? (int)(long)result : 0;
 
-        ++permId;
+            ++permId;
 
-        var account = new Account(permId, username, password);
+            var account = new Account(permId, username, password);
 
-        CreateAccount(account);
+            CreateAccount(account);
 
-        return account;
+            return account;
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
+        }
     }
 
     public static void CreateAccount(Account account)
     {
-        string insertQuery = @"
+        rwLock.EnterWriteLock();
+        try
+        {
+            string insertQuery = @"
                     INSERT INTO Accounts 
                     (PermId, Username, Password, Salt, Name, Nick, Clan, Page, Icq, Text, Email, ShowEmail) 
                     VALUES (@PermId, @Username, @Password, @Salt, @Name, @Nick, @Clan, @Page, @Icq, @Text, @Email, @ShowEmail)";
 
-        using var command = new SQLiteCommand(insertQuery, connection);
-        command.Parameters.AddWithValue("@PermId", account.PermId);
-        command.Parameters.AddWithValue("@Username", account.Username);
-        command.Parameters.AddWithValue("@Password", account.Password);
-        command.Parameters.AddWithValue("@Salt", account.Salt);
-        command.Parameters.AddWithValue("@Name", account.Name);
-        command.Parameters.AddWithValue("@Nick", account.Nick);
-        command.Parameters.AddWithValue("@Clan", account.Clan);
-        command.Parameters.AddWithValue("@Page", account.Page);
-        command.Parameters.AddWithValue("@Icq", account.Icq);
-        command.Parameters.AddWithValue("@Text", account.Text);
-        command.Parameters.AddWithValue("@Email", account.Email);
-        command.Parameters.AddWithValue("@ShowEmail", account.ShowEmail ? 1 : 0);
+            using var command = new SQLiteCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@PermId", account.PermId);
+            command.Parameters.AddWithValue("@Username", account.Username);
+            command.Parameters.AddWithValue("@Password", account.Password);
+            command.Parameters.AddWithValue("@Salt", account.Salt);
+            command.Parameters.AddWithValue("@Name", account.Name);
+            command.Parameters.AddWithValue("@Nick", account.Nick);
+            command.Parameters.AddWithValue("@Clan", account.Clan);
+            command.Parameters.AddWithValue("@Page", account.Page);
+            command.Parameters.AddWithValue("@Icq", account.Icq);
+            command.Parameters.AddWithValue("@Text", account.Text);
+            command.Parameters.AddWithValue("@Email", account.Email);
+            command.Parameters.AddWithValue("@ShowEmail", account.ShowEmail ? 1 : 0);
 
-        command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
+        }
     }
 
     public static bool TryGetAccount(string username, [NotNullWhen(true)] out Account? account)
     {
-        var query = "SELECT * FROM Accounts WHERE Username = @Username LIMIT 1";
-
-        using var command = new SQLiteCommand(query, connection);
-        command.Parameters.AddWithValue("@Username", username);
-
-        using var reader = command.ExecuteReader(System.Data.CommandBehavior.KeyInfo);
-
-        if (reader.Read())
+        rwLock.EnterReadLock();
+        try
         {
-            account = ReadAccount(reader);
-            return true;
+            var query = "SELECT * FROM Accounts WHERE Username = @Username LIMIT 1";
+
+            using var command = new SQLiteCommand(query, connection);
+            command.Parameters.AddWithValue("@Username", username);
+
+            using var reader = command.ExecuteReader(System.Data.CommandBehavior.KeyInfo);
+
+            if (reader.Read())
+            {
+                account = ReadAccount(reader);
+                return true;
+            }
+            else
+            {
+                account = null;
+                return false;
+            }
         }
-        else
+        finally
         {
-            account = null;
-            return false;
+            rwLock.ExitReadLock();
         }
     }
 
     public static bool TryGetAccount(int permId, [NotNullWhen(true)] out Account? account)
     {
-        var query = "SELECT * FROM Accounts WHERE PermId = @PermId LIMIT 1";
-
-        using var command = new SQLiteCommand(query, connection);
-        command.Parameters.AddWithValue("@PermId", permId);
-
-        using var reader = command.ExecuteReader(System.Data.CommandBehavior.KeyInfo);
-        if (reader.Read())
+        rwLock.EnterReadLock();
+        try
         {
-            account = ReadAccount(reader);
-            return true;
+            var query = "SELECT * FROM Accounts WHERE PermId = @PermId LIMIT 1";
+
+            using var command = new SQLiteCommand(query, connection);
+            command.Parameters.AddWithValue("@PermId", permId);
+
+            using var reader = command.ExecuteReader(System.Data.CommandBehavior.KeyInfo);
+            if (reader.Read())
+            {
+                account = ReadAccount(reader);
+                return true;
+            }
+            else
+            {
+                account = null;
+                return false;
+            }
         }
-        else
+        finally
         {
-            account = null;
-            return false;
+            rwLock.ExitReadLock();
         }
     }
 
     public static bool TryLogin(string username, string password, [NotNullWhen(true)] out Account? account)
     {
-        if (TryGetAccount(username, out account))
+        rwLock.EnterReadLock();
+        try
         {
-            return account.CheckPassword(password);
+            if (TryGetAccount(username, out account))
+            {
+                return account.CheckPassword(password);
+            }
+            else
+            {
+                account = null;
+                return false;
+            }
         }
-        else
+        finally
         {
-            account = null;
-            return false;
+            rwLock.ExitReadLock();
         }
     }
 
     public static void SetProfile(int permid, ProfileData profileData)
     {
-        string updateQuery = @"
+        rwLock.EnterWriteLock();
+        try
+        {
+            string updateQuery = @"
         UPDATE Accounts SET
             Name = @Name,
             Nick = @Nick,
@@ -145,18 +205,23 @@ public static class Database
             ShowEmail = @ShowEmail
         WHERE PermId = @PermId";
 
-        using var command = new SQLiteCommand(updateQuery, connection);
-        command.Parameters.AddWithValue("@PermId", permid);
-        command.Parameters.AddWithValue("@Name", profileData.Name);
-        command.Parameters.AddWithValue("@Nick", profileData.Nick);
-        command.Parameters.AddWithValue("@Clan", profileData.Clan);
-        command.Parameters.AddWithValue("@Page", profileData.Page);
-        command.Parameters.AddWithValue("@Icq", profileData.Icq);
-        command.Parameters.AddWithValue("@Text", profileData.Text);
-        command.Parameters.AddWithValue("@Email", profileData.Email);
-        command.Parameters.AddWithValue("@ShowEmail", profileData.ShowEmail ? 1 : 0);
+            using var command = new SQLiteCommand(updateQuery, connection);
+            command.Parameters.AddWithValue("@PermId", permid);
+            command.Parameters.AddWithValue("@Name", profileData.Name);
+            command.Parameters.AddWithValue("@Nick", profileData.Nick);
+            command.Parameters.AddWithValue("@Clan", profileData.Clan);
+            command.Parameters.AddWithValue("@Page", profileData.Page);
+            command.Parameters.AddWithValue("@Icq", profileData.Icq);
+            command.Parameters.AddWithValue("@Text", profileData.Text);
+            command.Parameters.AddWithValue("@Email", profileData.Email);
+            command.Parameters.AddWithValue("@ShowEmail", profileData.ShowEmail ? 1 : 0);
 
-        command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
+        }
     }
 
 
@@ -165,23 +230,39 @@ public static class Database
         if (!Config.Instance.StorePersistentData)
             return null;
 
-        var path = Path.Combine(Config.Instance.SavesPath, permId.ToString(), $"Hero{blockId - 1:D2}.pax");
+        rwLock.EnterReadLock();
+        try
+        {
+            var path = Path.Combine(Config.Instance.SavesPath, permId.ToString(), $"Hero{blockId - 1:D2}.pax");
 
-        if (!Path.Exists(path))
-            return null;
+            if (!Path.Exists(path))
+                return null;
 
-        return new SaveFile(path);
+            return new SaveFile(path);
+        }
+        finally
+        {
+            rwLock.ExitReadLock();
+        }
     }
 
     public static void SetSaveFile(int permId, int blockId, SaveFile saveFile)
     {
         if (!Config.Instance.StorePersistentData)
             return;
+    
+        rwLock.EnterWriteLock();
+        try
+        {
+            var path = Path.Combine(Config.Instance.SavesPath, permId.ToString(), $"Hero{blockId - 1:D2}.pax");
+            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
 
-        var path = Path.Combine(Config.Instance.SavesPath, permId.ToString(), $"Hero{blockId - 1:D2}.pax");
-        Directory.CreateDirectory(Path.GetDirectoryName(path)!);
-
-        saveFile.Save(path);
+            saveFile.Save(path);
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
+        }
     }
 
     public static void InitSaveFile(int permId, int blockId, int templateId, string name)
@@ -189,32 +270,40 @@ public static class Database
         if (!Config.Instance.StorePersistentData)
             return;
 
-        var path = Path.Combine(Config.Instance.SavesPath, permId.ToString(), $"Hero{blockId - 1:D2}.pax");
-
-        if (templateId == 16)
+        rwLock.EnterWriteLock();
+        try
         {
-            File.Delete(path);
-        }
-        else
-        {
-            var templatePath = Path.Combine(Config.Instance.TemplatePath, $"Hero{templateId:D2}.ptx");
-            var saveFile = new SaveFile(templatePath);
+            var path = Path.Combine(Config.Instance.SavesPath, permId.ToString(), $"Hero{blockId - 1:D2}.pax");
 
-            var preview = saveFile.GetCharacterPreview();
-
-            preview = preview with 
+            if (templateId == 16)
             {
-                Name = name
-            };
-
-            saveFile.SetCharacterPreview(preview);
-
-            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
-
-            if(File.Exists(path))
                 File.Delete(path);
+            }
+            else
+            {
+                var templatePath = Path.Combine(Config.Instance.TemplatePath, $"Hero{templateId:D2}.ptx");
+                var saveFile = new SaveFile(templatePath);
 
-            saveFile.Save(path);
+                var preview = saveFile.GetCharacterPreview();
+
+                preview = preview with
+                {
+                    Name = name
+                };
+
+                saveFile.SetCharacterPreview(preview);
+
+                Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+
+                if (File.Exists(path))
+                    File.Delete(path);
+
+                saveFile.Save(path);
+            }
+        }
+        finally
+        {
+            rwLock.ExitWriteLock();
         }
     }
 
