a95059b73a88317d69a934f0fa78d73536413328
Giulio Carlo D'Antonio <kde@kryotek.mozmail.com>
2025-03-30 18:43:00 +0200

    Feat: Named CharacterTypes

---
+++ src/Types/CharacterTypes.cs
@@ -0,0 +1,14 @@
+namespace Lobby.Types;
+
+public enum CharacterTypes : uint
+{
+    Seraphin = 1,
+    Gladiator = 2,
+    Mage = 3,
+    DarkElf = 4,
+    WoodElf = 5,
+    Vampire = 6,
+    VampireTransformed = 7,
+    Dwarf = 8,
+    Daemon = 9,
+}
